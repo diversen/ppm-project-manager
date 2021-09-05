73f571f8c0d773f8e47408f9cb0cfe076f12ef77
Dennis Iversen <dennis.iversen@gmail.com>
2021-09-05 15:46:57 +0200

    rm old autoload.php

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(function($className)
-{
-    $classPath = str_replace("\\", '/', $className) . '.php';
-    if (file_exists($classPath)) {
-        require_once $classPath;
-    }  
-});
