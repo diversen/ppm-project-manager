20e6dbe2eaccaedc6ce7765c43311ad737b956e7
Dennis Iversen <dennis.iversen@gmail.com>
2021-09-08 13:04:02 +0200

    clean up

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # README
 
-![logo.svg](logo.svg)
+![logo.svg](logo/logo.svg)
 
 PPM (PPM project mangement) is a single user project, task, and time management system. 
 
--- scripts/logo.sh
+++ scripts/logo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-text-to-svg-cli --config=logo.json
-cp logo.svg www/App/templates/assets/logo.svg
+text-to-svg-cli --config=logo/logo.json
+cp logo/logo.svg www/App/templates/assets/logo.svg
