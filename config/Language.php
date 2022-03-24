512baf4bb08909247c80dd8dc11c7730b1782a29
Dennis Iversen <dennis.iversen@gmail.com>
2022-03-24 18:52:27 +0100

    move translations

--- config/Language.php
+++ config/Language.php
@@ -11,7 +11,7 @@ return [
     'default' => 'en',
     
     // Directory were translation dir ('lang') exist 
-    'translate_base_dir' => 'www',
+    'translate_base_dir' => 'src',
 
     // For doing auto translations of languages enabled
     'google_application_credentials' => 'config-locale/pebble-xxx.json',
---
+++ src/lang/en/language.php
@@ -0,0 +1,402 @@
+<?php
+
+$LANG = array();
+
+// Translation of file www/App/Account/Controller.php
+
+$LANG['Signin'] = 'Signin';
+$LANG['You are logged in'] = 'You are logged in';
+$LANG['Wrong email or password. Or your account has not been activated.'] = 'Wrong email or password. Or your account has not been activated.';
+$LANG['Verify your login.'] = 'Verify your login.';
+$LANG['Signup'] = 'Signup';
+$LANG['Your account has been verified. You may log in'] = 'Your account has been verified. You may log in';
+$LANG['The key supplied has already been used'] = 'The key supplied has already been used';
+$LANG['Account has been created. You may log in'] = 'Account has been created. You may log in';
+$LANG['User created. An activation link has been sent to your email. Press the link and your account will be activated'] = 'User created. An activation link has been sent to your email. Press the link and your account will be activated';
+$LANG['The system could not create an account. Please try again another time'] = 'The system could not create an account. Please try again another time';
+$LANG['Recover account'] = 'Recover account';
+$LANG['Invalid Request. We will look in to this'] = 'Invalid Request. We will look in to this';
+$LANG['No such email in our system'] = 'No such email in our system';
+$LANG['The image text does not match your submission'] = 'The image text does not match your submission';
+$LANG['A notification email has been sent with instructions to create a new password'] = 'A notification email has been sent with instructions to create a new password';
+$LANG['E-mail could not be sent. Try again later.'] = 'E-mail could not be sent. Try again later.';
+$LANG['Your password has been updated'] = 'Your password has been updated';
+$LANG['Create new password'] = 'Create new password';
+$LANG['No such account connected to supplied key'] = 'No such account connected to supplied key';
+
+// Translation of file www/App/Account/ControllerExt.php
+
+$LANG['Signin'] = 'Signin';
+
+// Translation of file www/App/Account/Mail.php
+
+$LANG['Activation link'] = 'Activation link';
+$LANG['Recover your account'] = 'Recover your account';
+
+// Translation of file www/App/Account/Validate.php
+
+$LANG['Invalid Request. We will look in to this'] = 'Invalid Request. We will look in to this';
+$LANG['E-mail does already exists'] = 'E-mail does already exists';
+$LANG['Please enter a valid E-mail'] = 'Please enter a valid E-mail';
+$LANG['Passwords does not match'] = 'Passwords does not match';
+$LANG['Passwords should be at least 7 chars long'] = 'Passwords should be at least 7 chars long';
+$LANG['Image text does not match'] = 'Image text does not match';
+
+// Translation of file www/App/Account/views/menu.php
+
+$LANG['Email login'] = 'Email login';
+$LANG['Email signup'] = 'Email signup';
+$LANG['Lost password'] = 'Lost password';
+
+// Translation of file www/App/Account/views/newpassword.php
+
+$LANG['New password'] = 'New password';
+$LANG['Repeat new password'] = 'Repeat new password';
+$LANG['Send'] = 'Send';
+
+// Translation of file www/App/Account/views/recover.php
+
+$LANG['E-mail'] = 'E-mail';
+$LANG['Click to get a new image'] = 'Click to get a new image';
+$LANG['Enter above image text (click to get a new image). Case of the text does not matter'] = 'Enter above image text (click to get a new image). Case of the text does not matter';
+$LANG['Send'] = 'Send';
+
+// Translation of file www/App/Account/views/recover_email.php
+
+$LANG['Hi'] = 'Hi';
+$LANG['You have connected with the following site because you have lost your password'] = 'You have connected with the following site because you have lost your password';
+$LANG['If you have not requested a new password then you can just delete this message'] = 'If you have not requested a new password then you can just delete this message';
+$LANG['In order to reset your password you just have to press the following link - or copy and paste into your browsers URL line'] = 'In order to reset your password you just have to press the following link - or copy and paste into your browsers URL line';
+$LANG['Kind Regards'] = 'Kind Regards';
+
+// Translation of file www/App/Account/views/signin.php
+
+$LANG['By signing in you agree to the following terms of service, privacy policy, and disclaimer'] = 'By signing in you agree to the following terms of service, privacy policy, and disclaimer';
+$LANG['Terms of service'] = 'Terms of service';
+$LANG['Privacy policy'] = 'Privacy policy';
+$LANG['Disclaimer'] = 'Disclaimer';
+$LANG['E-mail'] = 'E-mail';
+$LANG['Password'] = 'Password';
+$LANG['Keep me signed in'] = 'Keep me signed in';
+$LANG['Send'] = 'Send';
+
+// Translation of file www/App/Account/views/signin_ext.php
+
+$LANG['By signing in you agree to the following terms of service, privacy policy, and disclaimer'] = 'By signing in you agree to the following terms of service, privacy policy, and disclaimer';
+$LANG['Terms of service'] = 'Terms of service';
+$LANG['Privacy policy'] = 'Privacy policy';
+$LANG['Disclaimer'] = 'Disclaimer';
+$LANG['E-mail'] = 'E-mail';
+$LANG['Password'] = 'Password';
+$LANG['Keep me signed in'] = 'Keep me signed in';
+$LANG['Send'] = 'Send';
+
+// Translation of file www/App/Account/views/signout.php
+
+$LANG['Logout'] = 'Logout';
+$LANG['You are logged in'] = 'You are logged in';
+$LANG['Sign out'] = 'Sign out';
+$LANG['Or'] = 'Or';
+$LANG['Sign out of all devices'] = 'Sign out of all devices';
+
+// Translation of file www/App/Account/views/signup.php
+
+$LANG['Signup'] = 'Signup';
+$LANG['E-mail'] = 'E-mail';
+$LANG['Password'] = 'Password';
+$LANG['Repeat password'] = 'Repeat password';
+$LANG['Click to get a new image'] = 'Click to get a new image';
+$LANG['Enter above image text (click to get a new image). Case of the text does not matter'] = 'Enter above image text (click to get a new image). Case of the text does not matter';
+$LANG['Send'] = 'Send';
+
+// Translation of file www/App/Account/views/signup_email.php
+
+$LANG['Hi'] = 'Hi';
+$LANG['You have connected with the following site and requested an account'] = 'You have connected with the following site and requested an account';
+$LANG['In order to activate your account, you just have to press the following link or copy and paste into your browsers URL line'] = 'In order to activate your account, you just have to press the following link or copy and paste into your browsers URL line';
+$LANG['Kind Regards'] = 'Kind Regards';
+
+// Translation of file www/App/AppACL.php
+
+$LANG['You are not logged in. Please log in.'] = 'You are not logged in. Please log in.';
+$LANG['You are not the owner of this project.'] = 'You are not the owner of this project.';
+$LANG['There is no such task ID'] = 'There is no such task ID';
+$LANG['There is no such time ID'] = 'There is no such time ID';
+
+// Translation of file www/App/Error/Controller.php
+
+$LANG['Page not found. If this page has existed then it has been deleted'] = 'Page not found. If this page has existed then it has been deleted';
+$LANG['404 Page not found'] = '404 Page not found';
+$LANG['403 Forbidden'] = '403 Forbidden';
+$LANG['500 Internal Server Error'] = '500 Internal Server Error';
+
+// Translation of file www/App/Google/Controller.php
+
+$LANG['Error trying to signin using Google'] = 'Error trying to signin using Google';
+$LANG['No ID token. Try again later'] = 'No ID token. Try again later';
+$LANG['Error trying to signin using Google. You will need to give this application access to your email and the email needs to be verified'] = 'Error trying to signin using Google. You will need to give this application access to your email and the email needs to be verified';
+$LANG['You are signed in.'] = 'You are signed in.';
+$LANG['Verify your login.'] = 'Verify your login.';
+
+// Translation of file www/App/Google/sign_in.tpl.php
+
+$LANG['Click the image and sign-in using google'] = 'Click the image and sign-in using google';
+
+// Translation of file www/App/Google/sign_out.tpl.php
+
+$LANG['You are signed in'] = 'You are signed in';
+$LANG['Sign out'] = 'Sign out';
+
+// Translation of file www/App/Overview/Controller.php
+
+$LANG['Overview'] = 'Overview';
+$LANG['Overview by week'] = 'Overview by week';
+
+// Translation of file www/App/Overview/overview.tpl.php
+
+$LANG['Total time used on project: '] = 'Total time used on project: ';
+$LANG['Edit task'] = 'Edit task';
+$LANG['Edit'] = 'Edit';
+$LANG['Add new task to project'] = 'Add new task to project';
+$LANG['New'] = 'New';
+$LANG['Add time to task'] = 'Add time to task';
+$LANG['Time'] = 'Time';
+$LANG['Move to today'] = 'Move to today';
+$LANG['Today'] = 'Today';
+$LANG['Your activity: <span class="notranslate">{activity}</span> '] = 'Your activity: <span class="notranslate">{activity}</span> ';
+$LANG['Task'] = 'Task';
+$LANG['Project'] = 'Project';
+$LANG['Week'] = 'Week';
+$LANG['Show week'] = 'Show week';
+$LANG['Current week'] = 'Current week';
+$LANG['Show full week'] = 'Show full week';
+$LANG['Show today'] = 'Show today';
+$LANG['Move exceeded to today'] = 'Move exceeded to today';
+$LANG['Activity this week: <span class="notranslate">{week_user_total}</span>'] = 'Activity this week: <span class="notranslate">{week_user_total}</span>';
+
+// Translation of file www/App/Project/Controller.php
+
+$LANG['All projects'] = 'All projects';
+$LANG['View project'] = 'View project';
+$LANG['Add project'] = 'Add project';
+$LANG['Edit project'] = 'Edit project';
+
+// Translation of file www/App/Project/ProjectModel.php
+
+$LANG['Title is required'] = 'Title is required';
+
+// Translation of file www/App/Project/views/project_add.tpl.php
+
+$LANG['Add project'] = 'Add project';
+$LANG['Title'] = 'Title';
+$LANG['Enter title'] = 'Enter title';
+$LANG['Note'] = 'Note';
+$LANG['Add an optional project note'] = 'Add an optional project note';
+$LANG['Add'] = 'Add';
+
+// Translation of file www/App/Project/views/project_edit.tpl.php
+
+$LANG['Projects'] = 'Projects';
+$LANG['Edit project'] = 'Edit project';
+$LANG['Title'] = 'Title';
+$LANG['Enter title'] = 'Enter title';
+$LANG['Add an optional project note'] = 'Add an optional project note';
+$LANG['Project is active'] = 'Project is active';
+$LANG['Update'] = 'Update';
+$LANG['Delete'] = 'Delete';
+$LANG['Are you sure want to delete this project? All tasks and all time entries will be deleted!'] = 'Are you sure want to delete this project? All tasks and all time entries will be deleted!';
+
+// Translation of file www/App/Project/views/project_index.tpl.php
+
+$LANG['Add project'] = 'Add project';
+$LANG['Your have no projects yet'] = 'Your have no projects yet';
+$LANG['Title'] = 'Title';
+$LANG['Date'] = 'Date';
+$LANG['Time used'] = 'Time used';
+$LANG['Edit'] = 'Edit';
+$LANG['New'] = 'New';
+$LANG['View inactive projects'] = 'View inactive projects';
+$LANG['Total time used on all projects'] = 'Total time used on all projects';
+
+// Translation of file www/App/Project/views/project_task_list.tpl.php
+
+$LANG['Edit task'] = 'Edit task';
+$LANG['Edit'] = 'Edit';
+$LANG['Add time to task'] = 'Add time to task';
+$LANG['Time'] = 'Time';
+$LANG['Move to today'] = 'Move to today';
+$LANG['Show more'] = 'Show more';
+
+// Translation of file www/App/Project/views/project_view.tpl.php
+
+$LANG['Projects'] = 'Projects';
+$LANG['Add task'] = 'Add task';
+$LANG['Total time used on project'] = 'Total time used on project';
+$LANG['Tasks waiting'] = 'Tasks waiting';
+$LANG['Task'] = 'Task';
+$LANG['Date'] = 'Date';
+$LANG['Time'] = 'Time';
+$LANG['Completed tasks'] = 'Completed tasks';
+
+// Translation of file www/App/Settings/Controller.php
+
+$LANG['Settings have been updated'] = 'Settings have been updated';
+$LANG['Your settings could not be saved. Check if you are logged in'] = 'Your settings could not be saved. Check if you are logged in';
+
+// Translation of file www/App/Settings/views/settings.tpl.php
+
+$LANG['Settings'] = 'Settings';
+$LANG['Two factor authentication'] = 'Two factor authentication';
+$LANG['Organization'] = 'Organization';
+$LANG['Your name'] = 'Your name';
+$LANG['Bio'] = 'Bio';
+$LANG['Add a bio'] = 'Add a bio';
+$LANG['Select your timezone'] = 'Select your timezone';
+$LANG['Select language'] = 'Select language';
+$LANG['Theme. Use dark mode'] = 'Theme. Use dark mode';
+$LANG['Update'] = 'Update';
+
+// Translation of file www/App/Settings/views/user.tpl.php
+
+$LANG['User has not entered any user info yet'] = 'User has not entered any user info yet';
+$LANG['User'] = 'User';
+$LANG['No user info'] = 'No user info';
+
+// Translation of file www/App/Task/TaskModel.php
+
+$LANG['Title is required'] = 'Title is required';
+$LANG['The end date can not be before the begin date'] = 'The end date can not be before the begin date';
+$LANG['There is no such task'] = 'There is no such task';
+
+// Translation of file www/App/Task/views/task_add.tpl.php
+
+$LANG['Projects'] = 'Projects';
+$LANG['Add task'] = 'Add task';
+$LANG['Title'] = 'Title';
+$LANG['Enter title'] = 'Enter title';
+$LANG['Add note'] = 'Add note';
+$LANG['Add an optional task note'] = 'Add an optional task note';
+$LANG['Priority'] = 'Priority';
+$LANG['Urgent'] = 'Urgent';
+$LANG['High'] = 'High';
+$LANG['Normal'] = 'Normal';
+$LANG['Minor'] = 'Minor';
+$LANG['Low'] = 'Low';
+$LANG['Task begin date'] = 'Task begin date';
+$LANG['Pick begin date'] = 'Pick begin date';
+$LANG['Task end date'] = 'Task end date';
+$LANG['Pick end date'] = 'Pick end date';
+$LANG['Submit'] = 'Submit';
+$LANG['Submit and stay'] = 'Submit and stay';
+
+// Translation of file www/App/Task/views/task_edit.tpl.php
+
+$LANG['Projects'] = 'Projects';
+$LANG['Edit task'] = 'Edit task';
+$LANG['Title'] = 'Title';
+$LANG['Enter title'] = 'Enter title';
+$LANG['Add note'] = 'Add note';
+$LANG['Add an optional task note'] = 'Add an optional task note';
+$LANG['Project'] = 'Project';
+$LANG['Priority'] = 'Priority';
+$LANG['Urgent'] = 'Urgent';
+$LANG['High'] = 'High';
+$LANG['Normal'] = 'Normal';
+$LANG['Minor'] = 'Minor';
+$LANG['Low'] = 'Low';
+$LANG['Task begin date'] = 'Task begin date';
+$LANG['Pick begin date'] = 'Pick begin date';
+$LANG['Task end date'] = 'Task end date';
+$LANG['Pick end date'] = 'Pick end date';
+$LANG['Submit'] = 'Submit';
+$LANG['Complete'] = 'Complete';
+$LANG['Open'] = 'Open';
+$LANG['Delete'] = 'Delete';
+$LANG['Complete this task?'] = 'Complete this task?';
+$LANG['Delete this task.Registered time entries will be removed?'] = 'Delete this task.Registered time entries will be removed?';
+$LANG['Open this task?'] = 'Open this task?';
+
+// Translation of file www/App/Task/views/task_view.tpl.php
+
+$LANG['Projects'] = 'Projects';
+$LANG['View task'] = 'View task';
+$LANG['Task'] = 'Task';
+$LANG['Project'] = 'Project';
+$LANG['Time'] = 'Time';
+$LANG['Edit task'] = 'Edit task';
+$LANG['Edit'] = 'Edit';
+$LANG['Add new task to project'] = 'Add new task to project';
+$LANG['New'] = 'New';
+$LANG['Add time to task'] = 'Add time to task';
+$LANG['Move to today'] = 'Move to today';
+$LANG['Today'] = 'Today';
+$LANG['Note'] = 'Note';
+
+// Translation of file www/App/Time/views/time_add.tpl.php
+
+$LANG['Add time'] = 'Add time';
+$LANG['Project'] = 'Project';
+$LANG['Time used. Valid time input (hh:mm), e.g. 1:10 or 0:15'] = 'Time used. Valid time input (hh:mm), e.g. 1:10 or 0:15';
+$LANG['Time used'] = 'Time used';
+$LANG['Note'] = 'Note';
+$LANG['Add an optional note'] = 'Add an optional note';
+$LANG['Pick date'] = 'Pick date';
+$LANG['Submit'] = 'Submit';
+$LANG['Submit and stay'] = 'Submit and stay';
+$LANG['Submit and close task'] = 'Submit and close task';
+$LANG['Time'] = 'Time';
+$LANG['Delete'] = 'Delete';
+$LANG['Are you sure you want to delete time entry?'] = 'Are you sure you want to delete time entry?';
+
+// Translation of file www/App/TranslateStr.php
+
+$LANG['A sever error happened. The incidence has been logged.'] = 'A sever error happened. The incidence has been logged.';
+
+// Translation of file www/App/TwoFactor/Controller.php
+
+$LANG['New QR code has been created'] = 'New QR code has been created';
+$LANG['The code could not be verified. Try again.'] = 'The code could not be verified. Try again.';
+$LANG['The code is verified. Two factor is enabled.'] = 'The code is verified. Two factor is enabled.';
+$LANG['You were to slow to enter two factor code. You will need to login again.'] = 'You were to slow to enter two factor code. You will need to login again.';
+$LANG['The code is verified. You are logged in.'] = 'The code is verified. You are logged in.';
+$LANG['You are signed in.'] = 'You are signed in.';
+
+// Translation of file www/App/TwoFactor/views/enable.tpl.php
+
+$LANG['Enable two factor authentication'] = 'Enable two factor authentication';
+$LANG['You will need a two factor app on your mobile phone'] = 'You will need a two factor app on your mobile phone';
+$LANG['Download a two factor app in your app store'] = 'Download a two factor app in your app store';
+$LANG['1. Scan the QR code to get started'] = '1. Scan the QR code to get started';
+$LANG['2. Enter code as seen on your phone'] = '2. Enter code as seen on your phone';
+$LANG['Submit'] = 'Submit';
+$LANG['You can use the following apps and many others'] = 'You can use the following apps and many others';
+
+// Translation of file www/App/TwoFactor/views/is_enabled.tpl.php
+
+$LANG['Enable two factor authentication'] = 'Enable two factor authentication';
+$LANG['Two factor is already enabled'] = 'Two factor is already enabled';
+$LANG['Get a new QR code'] = 'Get a new QR code';
+$LANG['If you create a new QR code then the old code will be deleted'] = 'If you create a new QR code then the old code will be deleted';
+
+// Translation of file www/App/TwoFactor/views/verify.tpl.php
+
+$LANG['Verify login using two factor authentication'] = 'Verify login using two factor authentication';
+$LANG['1. Enter code as seen on your phone'] = '1. Enter code as seen on your phone';
+$LANG['Submit'] = 'Submit';
+
+// Translation of file www/App/templates/main_menu.tpl.php
+
+$LANG['Login'] = 'Login';
+$LANG['Email signup'] = 'Email signup';
+$LANG['Lost password'] = 'Lost password';
+$LANG['Home'] = 'Home';
+$LANG['Projects'] = 'Projects';
+$LANG['Settings'] = 'Settings';
+$LANG['Sign out'] = 'Sign out';
+$LANG['Toggle timer'] = 'Toggle timer';
+$LANG['Timer'] = 'Timer';
+
+// Translation of file www/index.php
+
+$LANG['A sever error happened. The incidence has been logged.'] = 'A sever error happened. The incidence has been logged.';
+$LANG['A server error happened. The incidence has been logged.'] = 'A server error happened. The incidence has been logged.';
