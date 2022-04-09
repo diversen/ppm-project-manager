67a86cc1c40496dde173915a5cb3eed061efb215
Dennis Iversen <dennis.iversen@gmail.com>
2022-04-09 15:15:18 +0200

    css

--- src/AppMain.php
+++ src/AppMain.php
@@ -15,7 +15,7 @@ use App\Main\AppBase;
  */
 class AppMain extends AppBase
 {
-    public const VERSION = "1.2.1";
+    public const VERSION = "1.2.2";
     public static $nonce;
     public function sendHeaders()
     {
