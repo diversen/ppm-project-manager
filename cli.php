1fdcdd484be1e63ed48aee9a4849a07724ec3171
Dennis Iversen <dennis.iversen@gmail.com>
2022-07-02 14:34:54 +0200

    refactor. composer.lock

--- cli.php
+++ cli.php
@@ -7,16 +7,13 @@ use Pebble\CLI\User;
 use Pebble\CLI\DB;
 use Pebble\CLI\Migrate;
 use Pebble\CLI\Translate;
-use Pebble\Service\ConfigService;
-
-$config = (new ConfigService())->getConfig();
 
 $cli = new MinimalCli();
 $cli->commands = [
-    'user' => new User($config),
-    'db' => new DB($config),
-    'migrate' => new Migrate($config),
-    'translate' => new Translate($config),
+    'user' => new User(),
+    'db' => new DB(),
+    'migrate' => new Migrate(),
+    'translate' => new Translate(),
 ];
 
 $cli->runMain();
