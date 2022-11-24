f33cb024deb87dc320fc10482e110fb0b85f2d72
Dennis Iversen <dennis.iversen@gmail.com>
2022-11-24 10:38:08 +0100

    use stream handler as default log

--- config/Log.php
+++ config/Log.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 use Pebble\Path;
 use Monolog\Logger;
-use Monolog\Handler\RotatingFileHandler;
+
+use Monolog\Handler\StreamHandler;
 
 $logger = new Logger('base');
 $base_path = Path::getBasePath();
-
-$rotating_handler = new RotatingFileHandler($base_path . '/logs/main.log', 365, Logger::DEBUG);
-$logger->pushHandler($rotating_handler);
+$logger->pushHandler(new StreamHandler($base_path . '/logs/main.log', Logger::DEBUG));
 
 return [
     'logger' => $logger,
 ];
 
-// Above uses RotatingFileHandler, below uses StreamHandler
-// use Monolog\Handler\StreamHandler;
-// $logger->pushHandler(new StreamHandler($base_path . '/logs/main.log', Logger::DEBUG));
+// RotatingFileHandler
+// use Monolog\Handler\RotatingFileHandler;
+// $rotating_handler = new RotatingFileHandler($base_path . '/logs/main.log', 365, Logger::DEBUG);
+// $logger->pushHandler($rotating_handler);
+
+
