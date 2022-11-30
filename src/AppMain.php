78b7d191baeda5a875e098fc4bd18fec9d94d625
Dennis Iversen <dennis.iversen@gmail.com>
2022-11-30 21:08:20 +0100

    new version

--- src/AppMain.php
+++ src/AppMain.php
@@ -17,7 +17,7 @@ class AppMain extends AppBase
 {
     use \App\CSP;
 
-    public const VERSION = "1.7.1";
+    public const VERSION = "2.0.0";
 
     /**
      * Load user language and timezone if set else load default language
