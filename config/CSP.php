3eb9e6d7be6878930f6a2041ff910c80a3e4089b
Dennis Iversen <dennis.iversen@gmail.com>
2022-12-07 14:36:55 +0100

    mv csp to config

--- config/CSP.php
+++ config/CSP.php
@@ -1,5 +1,30 @@
 <?php
 
+use Aidantwoods\SecureHeaders\SecureHeaders;
+use Pebble\Random;
+
+$nonce = Random::generateRandomString(16);
+
+$headers = new SecureHeaders();
+$headers->strictMode(false);
+$headers->errorReporting(true);
+$headers->hsts();
+$headers->csp('default', 'self');
+$headers->csp('base-uri', 'self');
+$headers->csp('img-src', 'data:');
+$headers->csp('img-src', 'self');
+$headers->csp('script-src', "'nonce-$nonce'");
+$headers->csp('script-src', "https://*.googletagmanager.com");
+$headers->csp('img-src', 'https://*.google-analytics.com https://*.googletagmanager.com');
+$headers->csp('connect-src', 'https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com');
+$headers->csp('connect-src', 'self');
+$headers->csp('style-src', 'self');
+$headers->csp('style-src', 'https://cdnjs.cloudflare.com');
+$headers->csp('font-src', 'https://cdnjs.cloudflare.com');
+$headers->csp('worker-src', 'self');
+
 return [
     'enabled' => true,
+    'headers' => $headers,
+    'nonce' => $nonce,
 ];
--- src/Utils/CSP.php
+++ src/Utils/CSP.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace App\Utils;
 
 use Aidantwoods\SecureHeaders\SecureHeaders;
-
-use Pebble\Random;
 use Pebble\Service\ConfigService;
 
 trait CSP
@@ -26,30 +24,12 @@ trait CSP
             return;
         }
 
-        self::$nonce = Random::generateRandomString(16);
+        self::$nonce = $config->get('CSP.nonce');
 
-        $nonce = self::$nonce;
-
-        $headers = new SecureHeaders();
-        $headers->strictMode(false);
-        $headers->errorReporting(true);
-        $headers->hsts();
-        $headers->csp('default', 'self');
-        $headers->csp('base-uri', $config->get('App.server_url'));
-        $headers->csp('img-src', 'data:');
-        $headers->csp('img-src', $config->get('App.server_url'));
-        
-        $headers->csp('script-src', "'nonce-$nonce'");
-
-        $headers->csp('script-src', "https://*.googletagmanager.com");
-        $headers->csp('img-src', 'https://*.google-analytics.com https://*.googletagmanager.com');
-        $headers->csp('connect-src', 'https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com');
-        $headers->csp('connect-src', $config->get('App.server_url'));
-        $headers->csp('style-src', 'self');
-        $headers->csp('style-src', 'https://cdnjs.cloudflare.com');
-        $headers->csp('font-src', 'https://cdnjs.cloudflare.com');
-
-        $headers->csp('worker-src', $config->get('App.server_url'));
+        /**
+         * @var SecureHeaders $headers
+         */
+        $headers = $config->get('CSP.headers');
         $headers->apply();
     }
 
