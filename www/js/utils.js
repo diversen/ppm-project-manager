f0fda2317871908bffb9d62748858adba479f552
Dennis Iversen <dennis.iversen@gmail.com>
2022-12-25 11:56:37 +0100

    rm not used js

--- www/js/utils.js
+++
@@ -1,8 +0,0 @@
-function addEventListenerAll(elems, event, cb) {
-    const aryElems = Array.from(elems)
-    aryElems.forEach(elem => {
-        elem.addEventListener(event, cb)
-    })
-}
-
-export {addEventListenerAll}
