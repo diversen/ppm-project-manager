2e1cf4c5a7ac1c200bd22138bbb63336f4f21760
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-16 15:28:06 +0100

    clean up docker stuff

--- docker-files/Dockerfile.php
+++ docker-files/Dockerfile.php
@@ -1,9 +1,12 @@
 FROM php:8.1-cli
 RUN apt-get update
-RUN apt-get -y install zlib1g-dev libpng-dev libjpeg-dev libpng-dev libfreetype6-dev
+RUN apt-get -y install zlib1g-dev libpng-dev libjpeg-dev libpng-dev libfreetype6-dev default-mysql-client
 RUN docker-php-ext-configure gd --with-freetype --with-jpeg
 RUN docker-php-ext-install pdo_mysql gd
-COPY . /app
+RUN mkdir -p /app
+COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
+COPY ./serv.sh /app
+COPY ./cli.sh /app
 WORKDIR /app
-CMD [ "./serv"]
+CMD [ "./serv.sh"]
 EXPOSE 8000
--- tools/docker/rm.sh
+++ tools/docker/rm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Remove php-cli-server image
-docker rm php-cli-server
+docker rmi php-cli-server
 
 # Remove mysql-server container
 docker stop mysql-server
