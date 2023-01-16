fcbf2b2840c04affd52fe53531f8f658c11de80e
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-16 11:07:04 +0100

    version

--- src/AppMain.php
+++ src/AppMain.php
@@ -17,7 +17,7 @@ class AppMain extends AppBase
     use \Pebble\Trait\CSP;
     use \Pebble\Trait\CSRF;
 
-    public const VERSION = "v2.1.6-beta-3";
+    public const VERSION = "v2.1.6";
 
     public function run()
     {
