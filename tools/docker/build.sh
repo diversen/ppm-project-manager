6558b11ecd78e3616471cfba834306767c64c153
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-16 22:33:11 +0100

    move files a bit around

--- tools/docker/build.sh
+++ tools/docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Start
-docker build -f docker-files/Dockerfile.php  -t php-cli-server .
+docker build -f tools/docker/Dockerfile.php  -t php-cli-server .
 
 # Create and start
 docker run -p 3306:3306 --name mysql-server -e MYSQL_ROOT_PASSWORD=password -d mysql:8.0
