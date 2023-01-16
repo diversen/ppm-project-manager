de2cd7433fb57e221e5f8d4a64ceccf3231cfe71
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-16 14:57:41 +0100

    fix view path

--- src/Info/Controller.php
+++ src/Info/Controller.php
@@ -19,12 +19,13 @@ class Controller extends AppUtils
      */
     public function phpinfo()
     {
+        
         header("Content-Security-Policy: script-src: 'self' 'unsafe-inline'");
         $this->acl_role->hasRoleOrThrow(['right' => 'admin', 'auth_id' => $this->acl_role->getAuthId()]);
 
         $template_vars = [];
         $this->template->render(
-            'Admin/views/system.php',
+            'Info/views/system.php',
             $template_vars
         );
     }
