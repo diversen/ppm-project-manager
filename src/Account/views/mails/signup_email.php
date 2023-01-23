18318c6351d5d4ce59262fc987b5325d218cb518
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-23 10:32:59 +0100

    nicer signup

--- src/Account/views/mails/signup_email.php
+++ src/Account/views/mails/signup_email.php
@@ -5,9 +5,7 @@ use Diversen\Lang;
 ?>
 <?=Lang::translate('Hi')?>,
 
-<?=Lang::translate('You have connected with the following site and requested an account')?>:
-
-<?=$site_name?>
+<?=Lang::translate('You have connected with the following site and requested an account')?>: <?=$site_name?>
 
 <?=Lang::translate('In order to activate your account, you just have to press the following link or copy and paste into your browsers URL line')?>:
 
