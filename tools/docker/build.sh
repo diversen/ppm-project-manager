1e9e8e70a6c08b77cd11a50703a85a29d0fcb2a1
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-23 09:13:15 +0100

    some notes to docker commands

--- tools/docker/build.sh
+++ tools/docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Start
+# Build php-cli-server image
 docker build -f tools/docker/Dockerfile.php  -t php-cli-server .
 
-# Create and start
+# Run mysql-server container
 docker run -p 3306:3306 --name mysql-server -e MYSQL_ROOT_PASSWORD=password -d mysql:8.0
--- tools/docker/rm.sh
+++ tools/docker/rm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Remove php-cli-server image
+docker stop cli-server
 docker rmi php-cli-server
 
 # Remove mysql-server container
