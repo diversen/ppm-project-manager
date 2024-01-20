6df3990d46c1795c00b908a5ad0d1274bb4a912c
diversen <dennis.iversen@gmail.com>
2024-01-20 09:37:06 +0100

    some better notes about why the datetime system should work in different timezones

--- src/Utils/AppCal.php
+++ src/Utils/AppCal.php
@@ -13,6 +13,14 @@ class AppCal
     /**
      * Get an array containing the weekdays where the key is a unix timestamp
      * and the value is a date string formatted according to $format
+     * 
+     * Every key is a unix timestamp for the start of the day according to the
+     * timezone set by the user (a date starts on a different unix timestamp 
+     * depending on the timezone set by the user). 
+     * 
+     * As we are getting the current week days we are getting values like 
+     * '2024-01-15 00:00:00' and '2024-01-16 00:00:00'. This corresponds to
+     * UTC datetime strings used in the database, e.g. on 'task'. 
      */
     public function getCurrentWeekDays(int $week_delta, string $format = 'Y-m-d H:i:s')
     {
@@ -45,7 +53,7 @@ class AppCal
     }
 
     /**
-     * Get a week delta string, e.g. +1 week or -10 week
+     * Get a week delta string, e.g. +1 week or -10 week to be used in strtotime
      * @return string $week_delta_str a string representing that week that can be read by strtotime
      */
     public function getWeekDeltaStr(int $week_delta)
@@ -61,8 +69,10 @@ class AppCal
     }
 
     /**
-     * Get a week number as string from a week delta. e.g. -2 or +4
-     * indicating the week number calculated from the current week)
+     * Get the number of the week as a string from a week delta (e.g. -2 or +4)
+     * It is calculated using 'today $week_delta_str', e.g. 'today -2 week'
+     * This takes into account current unix time of the user and 
+     * the timezone set by the user
      */
     public function getWeekNumberFromDelta(int $week_delta)
     {
@@ -71,10 +81,12 @@ class AppCal
     }
 
 
+    /**
+     * Check if a timestamp is today
+     * This takes into account the timezone set by the user
+     */
     public function isToday($ts)
     {
-        //'strtotime' takes into account the timezone
-        // set by the user
         $today_ts = strtotime('today');
         $is_today = false;
         if ($today_ts == $ts) {
